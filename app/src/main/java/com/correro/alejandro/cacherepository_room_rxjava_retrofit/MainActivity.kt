8b66be17b7331c8268541d4e37a4255aaefbfa21
Alejandro Correro <acorrero@omniumlab.com>
2018-11-05 20:00:02 +0100

    Initial commit

---
+++ app/src/main/java/com/correro/alejandro/cacherepository_room_rxjava_retrofit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.correro.alejandro.cacherepository_room_rxjava_retrofit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
